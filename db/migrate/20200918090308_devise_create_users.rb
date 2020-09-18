414740ef485ccb9ccde008ab98c814657383023f
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-09-18 09:15:52 +0000

    [add]image_id to users table

--- db/migrate/20200918090308_devise_create_users.rb
+++ db/migrate/20200918090308_devise_create_users.rb
@@ -32,7 +32,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :name
+      t.string :name, null: false, default: ""
+      t.string :user_image_id
       t.timestamps null: false
     end
 
