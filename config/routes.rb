3c54528ec8157eddd3c34f5ea74a55401437996f
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-10-02 09:44:53 +0000

    [add]routes for posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 	root "homes#top"
 	get 'about' => 'home#about'
-	resources :posts, only: [:create, :index, :destroy]
+	resources :posts, only: [:create, :index, :show, :destroy]
 	resources :favorites, only: [:create, :index, :destroy]
 	resources :users, only: [:show,:edit,:update, :destroy]
 end
