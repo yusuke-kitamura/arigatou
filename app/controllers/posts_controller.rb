e3aa2ecbc4b33c28b4a974d56732cf926d67a249
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-10-03 02:57:24 +0000

    [add]to delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,8 @@ class PostsController < ApplicationController
 
 	def destroy
 		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to user_path, notice: "successfuly deleted"
 	end
 
 	private
