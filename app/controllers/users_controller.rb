8ac313c145cc3bcbb10b1e8c10fa6464cb311d8b
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-10-03 02:50:48 +0000

    [add]after updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,13 @@ class UsersController < ApplicationController
 	end
 
 	def edit
+		@user = current_user
 	end
 
 	def update
+		@user = current_user
+		@user.update
+		redirect_to edit_user_path, notice: "succesfully updated"
 	end
 
 	def destroy
