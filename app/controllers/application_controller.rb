648f524408ba1eed7c219720e9f52a75f7eacedf
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-10-07 02:37:49 +0000

    [add]to permit parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
 
-	protected
-
 	def after_sign_in_path_for(resource)
   	end
 
   	def after_sign_out_path_for(resource)
+  	end
+
+  	protected
+
+  	def configure_permitted_parameters
+	    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:email])
+	    devise_parameter_sanitizer.permit(:sign_in,keys:[:name,:email])
+	  	devise_parameter_sanitizer.permit(:account_update,keys:[:name,:email])
   	end
 end
